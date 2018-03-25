e3711688b9592474f0207242fb4eb6f952a67b0d
100297 <league@WTS14.attlocal.net>
2018-03-25 13:00:14 -0700

    wqe
    wqewq

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,25 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	public double divide(double a, double b) throws IllegalArgumentException {
+		if (b == 0) {
+			throw new IllegalArgumentException();
+		} else {
+			return a / b;
+		}
+
+	}
+
+	public String reverseString(String a1) {
+		String reverseWord = "";
+		if (a1 == "") {
+			throw new IllegalArgumentException();
+		} else {
+			for (int i = a1.length() - 1; i > -1; i++) {
+				reverseWord += a1.charAt(i);
+			}
+			return reverseWord;
+		}
+	}
+
 }
